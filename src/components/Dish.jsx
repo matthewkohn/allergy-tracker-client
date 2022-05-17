d052502abadb38f53b1a93addc4d7c37ff67256a
MattKohn <kohney@hotmail.com>
2022-05-17 16:04:47 -0600

    Build Dish cards, style Dishes component container

--- src/components/Dish.jsx
+++ src/components/Dish.jsx
@@ -1,9 +1,30 @@
 import React from 'react'
+import { Card, Typography } from '@mui/material'
+import { styled } from '@mui/material/styles'
 
 const Dish = () => {
   return (
-    <div>Dish</div>
+    <DishCard>
+      <Typography variant="h6">Really Long Dish Name</Typography>
+      <Details variant="caption">Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda cumque ipsa reprehenderit qui vero facilis, ratione vitae amet. Totam cupiditate velix Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam cupiditate velix</Details>
+      <Details variant="subtitle">$29.99</Details>
+    </DishCard>
   )
 }
 
 export default Dish
+
+const DishCard = styled(Card)({
+  display: 'flex',
+  flexDirection: 'column',
+  margin: '10px 30px',
+  padding: '20px',
+  width: '200px',
+  height: '300px'
+})
+
+const Details = styled(Typography)({
+  padding: '10px 0',
+  overflow: 'auto',
+  color: 'grey'
+})
--- src/components/Dishes.jsx
+++ src/components/Dishes.jsx
@@ -1,12 +1,25 @@
 import React from 'react'
 import Dish from './Dish'
+import { Container } from '@mui/material'
+import { styled } from '@mui/material/styles'
 
 const Dishes = () => {
   return (
-    <>
+    <DishContainer maxWidth="xl">
       <Dish />
-    </>
+      <Dish />
+      <Dish />
+      <Dish />
+      <Dish />
+    </DishContainer>
   )
 }
 
 export default Dishes
+
+const DishContainer = styled(Container)({
+  marginTop: '100px',
+  display: 'flex',
+  flexDirection: 'row',
+  flexWrap: 'wrap'
+})
