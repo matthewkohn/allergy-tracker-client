e64c1942bba06239c7567cff0331bbf53ed70b44
MattKohn <kohney@hotmail.com>
2022-05-17 15:24:08 -0600

    Add routes to App component with AppBar as the header

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,12 +2,20 @@ import React from 'react'
 import Dishes from './Dishes'
 import DishForm from './DishForm'
 import { Container } from '@mui/system'
+import { Routes, Route } from 'react-router-dom'
+import { AppBar, Typography } from '@mui/material'
 
 const App = () => {
   return (
     <Container>
-      <Dishes />
-      <DishForm />
+      <AppBar>
+        <Typography variant="h3">Cliff's Notes for Food Servers</Typography>
+      </AppBar>
+      <Routes>
+        <Route path='/' element={ <Dishes /> } />
+        <Route path='/new' element={ <DishForm /> } />
+        <Route path='/update' element={ <DishForm /> } />
+      </Routes>
     </Container>
   )
 }
